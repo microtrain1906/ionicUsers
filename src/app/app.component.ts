a7657b44457593688a714b0e962dd1e1f11ff521
chad-svastisalee <swasgrafx@gmail.com>
2019-08-13 10:28:16 -0500

    Added Login Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,21 +6,15 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
   public appPages = [
     {
-      title: 'Home',
-      url: '/home',
-      icon: 'home'
+      title: 'Login',
+      url: '/login',
+      icon: 'log-in'
     },
-    {
-      title: 'List',
-      url: '/list',
-      icon: 'list'
-    }
   ];
 
   constructor(
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms'; 
+
+import { UserService } from '../user.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  user:User = new User();
+  errorMessage: string;
 
-  ngOnInit() {
+  constructor(
+    private userService: UserService
+  ) {}
+
+  ngOnInit() {}
+
+  response(response): void{
+    if(response.success===false){
+      this.errorMessage = 'Invalid Credentials';
+    }
+
+    if(response.success===true){
+      window.location.href = '/';
+    }
+  }
+
+  onSubmit(): void{
+    this.userService.logIn(this.user).subscribe(
+      (response) => {
+        this.response(response);
+      }
+    );
   }
 
 }
