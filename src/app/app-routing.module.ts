f649997d388e4fc40203b7f379521a99f67653b2
chad-svastisalee <swasgrafx@gmail.com>
2019-08-15 09:55:10 -0500

    Added User Create Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,8 @@ const routes: Routes = [
   { path: 'logout', loadChildren: './logout/logout.module#LogoutPageModule' },
   { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
   { path: 'users', loadChildren: './users/users.module#UsersPageModule' },
-  { path: 'user/:id', loadChildren: './user/user.module#UserPageModule' }
+  { path: 'user/:id', loadChildren: './user/user.module#UserPageModule' },
+  { path: 'user-create', loadChildren: './user-create/user-create.module#UserCreatePageModule' }
 ];
 
 @NgModule({
